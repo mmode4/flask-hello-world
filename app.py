de3eb83eeb8fd06473d20d7abf2a428fc13dbfa2
mmode4 <103876393+mmode4@users.noreply.github.com>
2022-04-27 10:10:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from telegram.ext.updater import Updater
 from flask import Flask
 app = Flask(__name__)
-updater = Updater("5243900708:AAFo-DUpJ64SoEds4DqzNQtAUIKxyxTSruQ",
-                  use_context=True)
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
